6a6b17221dc0a28693e5fbd9a7fb198d960785b7
Stefan Braunovic <stefan.braunovic55@gmail.com>
2021-06-07 16:34:03 +0200

    Initial commit, Login component & style(animation)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from './pages/login/Login'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <h1>Rent A Car</h1> */}
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
